ef3fcbcd0e93bd252fd7fac7bff3d2bdd397fc31
AFRIPIMALL GVC <ahmadlateef298@gmail.com>
2026-09-03 23:32:19 +0100

    Fix Render deployment migration step

--- build.sh
+++ build.sh
@@ -6,6 +6,5 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py bootstrap_admin
